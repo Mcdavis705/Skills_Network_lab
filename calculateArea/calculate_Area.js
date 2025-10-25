224cc88fb79aaec57ebb488466c87ac78e56855e
mcdavis705 <mcdavis705@gmail.com>
2025-10-25 08:00:47 -0400

    fixed error boundary

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,15 +2,14 @@ let length;
 let width;
 
 function calculateArea() {
-    if (length,width){
     length = parseFloat(document.getElementById("length").value);
     width = parseFloat(document.getElementById("width").value);
-
-    let area = length * width;
-    document.getElementById("result").innerText = `The area of the rectangle is ${area}`
+    if (length && width){
+        let area = length * width;
+        document.getElementById("result").innerText = `The area of the rectangle is ${area}`
     } else {
         document.getElementById("result").style.color="red"
-        document.getElementById("result").innerText = 'input a valid width and length'
+        document.getElementById("result").innerText = 'The input fields cannot be empty'
     }
 }
 
