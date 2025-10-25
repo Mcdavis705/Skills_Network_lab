29524ade1715ace30e413cdc37fcb80a043d2a57
mcdavis705 <mcdavis705@gmail.com>
2025-10-25 06:48:34 -0400

    ternary

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -42,3 +42,8 @@ switch (userType){
     default:
         userCategory = "Unknown";
 }
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus)
