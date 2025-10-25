8b5ea42e86356ade8053886b8830f14610747801
mcdavis705 <mcdavis705@gmail.com>
2025-10-25 08:28:41 -0400

    fixed error boundary

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -13,4 +13,25 @@ function calculateArea() {
     }
 }
 
+let grocery1Amount = 0;
+let grocery2Amount = 0;
+let grocery3Amount = 0;
+
+function calculateTotalAmount(){
+    grocery1Amount = parseFloat(document.getElementById('grocery1').value);
+    grocery2Amount = parseFloat(document.getElementById('grocery2').value);
+    grocery3Amount = parseFloat(document.getElementById('grocery3').value);
+
+    if(isNaN(grocery1Amount) || isNaN(grocery2Amount) || isNaN(grocery3Amount)) {
+        grocery1Amount = isNaN(grocery1Amount) ? 0 : grocery1Amount;
+        grocery2Amount = isNaN(grocery2Amount) ? 0 : grocery2Amount;
+        grocery3Amount = isNaN(grocery3Amount) ? 0 : grocery3Amount;
+
+
+        let totalAmount = grocery1Amount + grocery2Amount + grocery3Amount
+        document.getElementById('totalAmount').innerText = `The total amount is: $${totalAmount}` 
+    } else {
+        alert("There are no groceries in your basket")
+    }
+}
 
