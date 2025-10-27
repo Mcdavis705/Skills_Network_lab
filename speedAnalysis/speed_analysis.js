c8374510d3c12d0379a1494d910a21b084e846e2
Mcdavis705 <mcdavis705@gmail.com>
2025-10-27 08:56:18 -0400

    letters count

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -32,6 +32,8 @@ function endTest() {
             return word !== "";
         }).length;
 
+        var typedLettersLength = userTypedText.length;
+        
         var wpm = 0; // Default value
 
         if (timeElapsed !== 0 && !isNaN(typedWords)) {
@@ -41,7 +43,9 @@ function endTest() {
         // Display the results
         var outputDiv = document.getElementById("output");
         outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+            "<p>Total length: " + typedLettersLength + "</p>" +
             "<p>Words Typed: " + typedWords + "</p>" +
             "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
             "<p>Words Per Minute (WPM): " + wpm + "</p>";
         }
+
